df802690eddf34c1cdc9919d0db33e2eb979b576
github username <github email address>
2018-10-10 13:03:41 -0400

    school commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, student_grade)
+    if @roster[student_grade] == nil
+      @roster[student_grade] = []
+    end
+    @roster[student_grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      @roster[grade] = students.sort
+    end
+  end
+end

d5e49ed7433306d2f5d90f27eaedd8c4c1314829
CarsonRey <c.rey494@gmail.com>
2018-10-10 22:34:14 -0400

    made school class with behavior of adding students, retreiving students from a grade, and sorting through students

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+class School
+  attr_writer
+  attr_accessor
+  attr_reader :roster, :sort
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student=(name, grade)
+    @name = name
+    @grade = grade
+  end
+  def add_student(name, grade)
+    if !@roster[grade]
+    @roster[grade] = []
+    @roster[grade] << name
+    else
+    @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student_array|
+      student_array.sort_by! do |student|
+        student[0]
+      end
+    end
+  end
+end

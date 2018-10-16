59c558b571b47bcd495a1caf8402590046ca82fe
github username <github email address>
2018-10-16 15:35:13 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :name, :age
+  def initialize(name)
+    @name = name
+    @age = age
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    # binding.pry
+    if !@roster.has_key?(grade)    # => check if the student's grade already exit in the {}.
+      @roster[grade] = [student]   # => {10=>["AC Slater"]}
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster[grade].sort
+  end
+
+end
